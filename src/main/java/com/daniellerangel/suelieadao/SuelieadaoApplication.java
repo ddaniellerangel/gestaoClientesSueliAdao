a338bb40e88731c36bd7631667afdc3498769ec5
Danielle Rangel <danielle1912@outlook.com>
2021-01-13 18:28:34 -0300

    Projeto criado

---
+++ src/main/java/com/daniellerangel/suelieadao/SuelieadaoApplication.java
@@ -0,0 +1,13 @@
+package com.daniellerangel.suelieadao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuelieadaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuelieadaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daniellerangel/suelieadao/SuelieadaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniellerangel.suelieadao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuelieadaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
